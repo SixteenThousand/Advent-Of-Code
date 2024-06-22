f64895ffaa6fae5f3f531bedda7e2151a6bca98c
Thomas Lindop <thomas16256@gmail.com>
2024-06-22 20:06:27 +0100

    >> ignore .yonax files >> continue work on day14 2020 > some debugging etc. >> refactor day14 2020 > remove any 'long long''s > actually use correct foramt specifier for long

--- 2020/Day14.c
+++ 2020/Day14.c
@@ -34,23 +34,28 @@ void add_loc(long address, long value) {
     memory_size++;
 }
 
-long long sum() {
-    long long result = 0;
+long sum() {
+    long result = 0;
+    char buffer[50]; // debug
+    void longtobinstr(long,char*,int); // debug
     for(int i=0; i<memory_size; i++) {
         result += memory[i].value;
+        longtobinstr(result,buffer,36); // debug
+        printf("Addr: %ld, Val: >>%s<<\n",memory[i].address,buffer); // debug
     }
     return result;
 }
     
 
-long long run(char *path) {
+long run(char *path) {
     char mask[LINE_LEN];
     long mask0;
     long mask1;
     long power = 1;
     long address;
     long number;
-    FILE *fp, *fopen();
+    FILE *fp, *fopen(const char*, const char*);
+    int fclose(FILE*);
     fp = fopen(path,"r");
     while (feof(fp) == 0) {
         fgets(line,LINE_LEN,fp);
@@ -77,10 +82,10 @@ long long run(char *path) {
                 i++;
                 power <<= 1;
             }
-            printf("given: %s\nmask0: %d\nmask1: %d\n",mask,mask0,mask1);  // debug
+            // printf("given: %s\nmask0: %ld\nmask1: %ld\n",mask,mask0,mask1);  // debug
         } else if(line[1] == 'e') {
             // mem stuff
-            sscanf(line,"mem[%d] = %d",&address,&number);
+            sscanf(line,"mem[%ld] = %ld",&address,&number);
             number = number | mask1;
             number = number & mask0;
             add_loc(address,number);
@@ -92,14 +97,16 @@ long long run(char *path) {
     return 0;
 }
 
+/**
+ * Converts a given number into a string, in binary form.
+ *     @param num: the given number
+ *     @param binstr: a character array. this is where the string will be put
+ *     @param width: the expected width of num in binary, at most the size
+ *     of binstr minus one. if num is too big for its binary form to fit in
+ *     width characters, then only the first {width}th characters will be
+ *     included. if it is smaller, there will be leading zeroes.
+ */
 void longtobinstr(long num, char *binstr, int width) {
-    // converts a given number into a string, in binary form
-    // num: the given number
-    // binstr: a character array. this is where the string will be put
-    // width: the expected width of num in binary, at most the size of binstr 
-    // minus one. if num is too big for its binary form to fit in
-    // width characters, then only the first {width}th characters will be 
-    // included. if it is smaller, there will be leading zeroes.
     *(binstr + width) = '\0';
     char *bufref = binstr + width - 1;
     while(bufref - binstr >= 0) {
@@ -115,6 +122,6 @@ void longtobinstr(long num, char *binstr, int width) {
 
 
 int main() {
-    run("../zeno.txt");
-    printf("%d\n",sum());
+    run("./gija");
+    printf("%ld\n",sum());
 }
